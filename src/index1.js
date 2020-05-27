5ade9fa0bcfe3eae48bf4d975eee37b091486f65
Pavel Maletin <maletinchess>
2020-05-27 14:41:47 +0700

    to change

--- src/index1.js
+++ src/index1.js
@@ -2,3 +2,8 @@ import _ from 'lodash';
 const result = _.head([1, 2, 3]);
 console.log(result);
 console.log(_.uniq([1, 1, 3]));
+const x = (coll) => {
+  const result = coll.map((item) => Math.sqrt(item));
+  return result;
+}
+console.log(x([25, 16, 9, 4, 1]));
